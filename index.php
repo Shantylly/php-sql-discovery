d2d98a47c0b359e3903ebf7053f3f954664d068b
Shanty <mickael.grezes@epitech.eu>
2022-08-03 15:05:13 +0200

    Separate style and added open text thing

--- index.php
+++ index.php
@@ -3,24 +3,19 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css">
         <title> PhP test </title>
     </head>
-    <style>
-        .block1 {
-        width: 400px;
-        padding: 10px;
-        border: 5px solid gray;
-        margin: 0;
-        }
-        .block2 {
-        width: 400px;
-        padding: 10px;
-        border: 5px solid gray;
-        margin: 0;
-        }
-    </style>
     <body>
-        <h1> Site test for PhP & MySQL </h1>
+        <nav class="center">
+            <h1> Site test for PhP & MySQL </h1>
+            <div>
+                <a href="#">Home</a>
+                <a href="#text">Traitement de fichier</a>
+                <a href="#sql">MySQL</a>
+                <a href="#dolibarr">Dolibarr</a>
+            </div>
+        </nav>
         <div class="block1">
             <?php
             $user = $_SERVER['HTTP_USER_AGENT'];
@@ -36,6 +31,14 @@
             else
                 echo 'Navigateur: ', $match[0], '<br /> ';
             ?>
+        </div>
+        <div class="block1">
+            <?php
+                    $fp = fopen ("exemple.txt", "r");
+                    $contenu_du_fichier = fgets ($fp, 255);
+                    fclose ($fp);
+                    echo 'Notre fichier contient : '.$contenu_du_fichier;
+                ?>
         </div>
     </body>
 </html>
